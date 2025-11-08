9ae03819441e5d179f0b683826155a11a144e717
Zeng G.Y. <samkuler@qq.com>
2025-11-08 13:08:34 +0800

    fix(client): Update import statements to use relative imports

--- Socket Programming/client/src/ftp/core/__init__.py
+++ Socket Programming/client/src/ftp/core/__init__.py
@@ -1,5 +1,5 @@
-from commands import CommandRegistry
-from connection import ControlConnection, DataConnection
-from parser import ResponseParser, FTPResponse
+from .commands import CommandRegistry
+from .connection import ControlConnection, DataConnection
+from .parser import ResponseParser, FTPResponse
 
 __all__ = ['CommandRegistry', 'ControlConnection', 'DataConnection', 'ResponseParser', 'FTPResponse']
